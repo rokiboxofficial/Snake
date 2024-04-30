87ad53d9731c61517966b633a8f1a42b45016564
Kirill <sosisks888@mail.ru>
2024-04-30 20:57:53 +0300

    add console messages

---
+++ src/Snake.UI/ConsoleSpinner.cs
@@ -0,0 +1,41 @@
+﻿namespace Snake.UI
+{
+    internal class ConsoleSpinner
+    {
+        private readonly string[,] sequence = null;
+
+        public int Delay { get; set; } = 400;
+
+        int totalSequences = 0;
+        int counter;
+
+        public ConsoleSpinner()
+        {
+            counter = 0;
+            sequence = new string[,]
+            {
+                {"   ", ".  ", ".. ", "..." }
+            };
+
+            totalSequences = sequence.GetLength(0);
+        }
+
+        public void GetAnimation(string displayMsg = "", int sequenceCode = 0)
+        {
+            counter++;
+
+            Thread.Sleep(Delay);
+
+            sequenceCode = sequenceCode > totalSequences - 1 ? 0 : sequenceCode;
+
+            int counterValue = counter % 4;
+
+            string fullMessage = displayMsg + sequence[sequenceCode, counterValue];
+            int msglength = fullMessage.Length;
+
+            Console.Write(fullMessage);
+
+            Console.SetCursorPosition(Console.CursorLeft - msglength, Console.CursorTop);
+        }
+    }
+}
--- src/Snake.UI/Program.cs
+++ src/Snake.UI/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Snake.UI
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var message = new ConsoleMessages();
+            var action = new UserActions();
+
+            message.InitialCommand();
+            message.WriteStartCommadnInstructionMessage();
+            message.WriteStopCommandInstructionMessage();
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                if (command == "-start")
+                {
+                    action.Start();
+                }
+
+                else if (command == "-stop")
+                {
+                    action.Stop();
+                    break;
+                }
+
+                else
+                {
+                    message.WriteErrorCommandMessage();
+                }
+            }
+        }
+    }
+}
