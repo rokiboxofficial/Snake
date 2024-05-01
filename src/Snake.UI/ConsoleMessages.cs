0b43609f255737baa221d43cc78329bc1924240a
NetMonster <sosisks888@mail.ru>
2024-05-01 11:44:34 +0300

    add console messages (#2)

---
+++ src/Snake.UI/ConsoleMessages.cs
@@ -0,0 +1,28 @@
+﻿namespace Snake.UI
+{
+    internal class ConsoleMessages
+    {
+        public void InitialCommand()
+        {
+            Console.WriteLine("Привет! Введите пожалуйста своё имя!");
+            var name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Вы зашли под именем {name}");
+        }
+
+        public void WriteStartCommadnInstructionMessage()
+        {
+            Console.WriteLine("Введите -start, что бы начать поиск игры");
+        }
+
+        public void WriteStopCommandInstructionMessage()
+        {
+            Console.WriteLine("Введите -stop, что бы прекратить поиск игры");
+        }
+
+        public void WriteErrorCommandMessage()
+        {
+            Console.WriteLine("Error! Команада не найдена!");
+        }
+    }
+}
---
+++ src/Snake.UI/UserActions.cs
@@ -0,0 +1,23 @@
+﻿namespace Snake.UI
+{
+    public class UserActions
+    {
+        public void Start()
+        {
+            Console.Clear();
+
+            ConsoleSpinner spinner = new ConsoleSpinner();
+
+            while (true)
+            {
+                spinner.GetAnimation(displayMsg: "Пожалуйста подождите", sequenceCode: 1);
+            }
+        }
+
+        public void Stop()
+        {
+            Console.Clear();
+            Console.WriteLine("Stop");
+        }
+    }
+}
